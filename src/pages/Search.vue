5285524066a13c04f019ecdd44c5f2c1a264f616
LinZeyuan <903586678@qq.com>
2025-10-28 17:41:25 +0800

    second_commit

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -1,10 +1,42 @@
 <script setup lang="ts">
-import {ref} from "vue";
-import {showToast} from "vant";
+import {ref} from "vue"
 
-const value = ref('');
-const onSearch = (val: any) => showToast(`搜索：${val || '无'}`);
-const onCancel = () => showToast('取消');
+// van-search
+const value = ref('')
+const onSearch = (val) => {
+  val.flatMap(parentTag => parentTag?.children).filter()
+}
+const onCancel = () => {
+  activeIds.value = []
+}
+
+// van-tree-select
+const activeIds = ref([]);
+const activeIndex = ref(0);
+const tagsList = [
+  {
+    text: '性别',
+    children: [
+      {text: '男', id: '男'},
+      {text: '女', id: '女'},
+      {text: '无', id: '无', disabled: true},
+    ],
+  },
+  {
+    text: '年级',
+    children: [
+      {text: '高三', id: '高三'},
+      {text: '大三', id: '大三'},
+      {text: '初三', id: '初三'},
+      {text: '初三', id: '初三a'},
+      {text: '初三', id: '初三b'},
+      {text: '初三', id: '初三c'},
+    ],
+  }
+];
+const doClose = (tag: any) => {
+  activeIds.value = activeIds.value.filter(item => item !== tag)
+}
 </script>
 
 <template>
@@ -16,8 +48,22 @@ const onCancel = () => showToast('取消');
       @search="onSearch"
       @cancel="onCancel"
   />
+  <van-divider content-position="left">已选标签</van-divider>
+  <div v-if="activeIds.length===0">请选择标签</div>
+  <van-row gutter="20">
+    <van-col v-for="tag in activeIds" style="padding: 10px">
+      <van-tag closeable size="medium" type="primary" @close="doClose(tag)">
+        {{ tag }}
+      </van-tag>
+    </van-col>
+  </van-row>
+  <van-divider content-position="left">选择标签</van-divider>
+  <van-tree-select
+      v-model:active-id="activeIds"
+      v-model:main-active-index="activeIndex"
+      :items="tagsList"
+  />
 </template>
-
 <style scoped>
 
 </style>
